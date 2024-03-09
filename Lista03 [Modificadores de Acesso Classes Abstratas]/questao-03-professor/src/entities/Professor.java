b2effc5ce583b4c179a5a2434a4a222748a3067d
BRADDTELES <braddteles@gmail.com>
2024-03-09 15:38:54 -0300

    adicionando o Getters e Setters e toString

--- Lista03 [Modificadores de Acesso Classes Abstratas]/questao-03-professor/src/entities/Professor.java
+++ Lista03 [Modificadores de Acesso Classes Abstratas]/questao-03-professor/src/entities/Professor.java
@@ -1,8 +1,47 @@
+package entities;
+
 public class Professor {
 
     private String nome;
     private int idade;
     private String disciplina;
     private String turma;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getDisciplina() {
+        return disciplina;
+    }
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    public String getTurma() {
+        return turma;
+    }
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor [nome: " 
+                + nome 
+                + ", idade: " 
+                + idade 
+                + ", disciplina: " 
+                + disciplina 
+                + ", turma: " 
+                + turma;
+    }
     
 }
